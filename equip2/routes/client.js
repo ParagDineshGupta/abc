7787dc87800e719378cba460c1273841af7a769e
ParagDineshGupta <guptaparag1996@gmail.com>
2018-06-07 12:09:32 +0530

    Update client.js

--- equip2/routes/client.js
+++ equip2/routes/client.js
@@ -1,18 +1,7 @@
 var url=require('url');
 var mysql = require('mysql');
 var qs=require('querystring');
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "experimental"
 
-});
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('conected');
-
-});
 
 var express = require('express');
 var router = express.Router();
