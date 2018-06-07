6315791f19b01bd11d172e32ec461a5ce7b56cd5
ParagDineshGupta <guptaparag1996@gmail.com>
2018-06-07 12:06:39 +0530

    Update company.js

--- equip2/routes/company.js
+++ equip2/routes/company.js
@@ -3,16 +3,8 @@ var router = express.Router();
 var fs = require('fs');
 var url=require('url');
 var mysql = require('mysql');
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "experimental"
-
-});
-con.connect(function(err) {
-    if (err) throw err;
-});
+var dbcon = require('./dbcon');
+var con = mysql.createConnection(dbcon.con);
 
 router.get('/', function(req, res, next) {
 	
